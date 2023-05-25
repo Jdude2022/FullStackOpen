89febcbf760f8b5d3d3bd250637f051eb91d53ea
Your Name <you@example.com>
2023-05-25 12:06:24 -0500

    to lesson 1.d

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -64,18 +64,73 @@
 //   )
 // }
 
-import { useState } from 'react'
+// import { useState } from 'react'
 
+//   const Display = ({ counter }) => <div>{counter}</div>
+
+//   const Button = ({handleClick, text}) => (
+//     <button onClick={handleClick}>
+//       {text}
+//       </button>
+//   )
+
+//   const App = () => {
+//   const [ counter, setCounter] = useState(0)
+//   console.log('rendering with counter value', counter)
+
+//   const increaseByOne = () => {
+//     console.log('increasing, value before', counter)
+//     setCounter(counter + 1)
+//   }
+
+//   const decreaseByOne = () => {
+//     console.log('deacreasing, value before', counter)
+//     setCounter(counter - 1)
+//   }
+
+//   const setToZero = () => {
+//     console.log('resetting to zero, value before', counter)
+//     setCounter(0)
+//   }
+
+//   return (
+//     <div>
+//       <Display counter={counter}/>
+//          <Button
+//          handleClick={increaseByOne}
+//          text='plus'
+//          /> 
+//          <Button
+//          handleClick={setToZero}
+//          text='zero'
+//          /> 
+//          <Button
+//          handleClick={decreaseByOne}
+//          text='minus'
+//          /> 
+  
+//     </div>
+//   )
+// }
+import { useState  } from "react"
 const App = () => {
-  const [ counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  return (
-    <div>{counter}</div>
+  const handleLeftClick = () => 
+      setClicks({ ...clicks, left: clicks.left + 1})
+ 
+  const handleRightCLick = () => 
+    setClicks({...clicks, right: clicks.right + 1 })
+  
+    return(
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightCLick}>right</button>
+      {clicks.right}
+    </div>
   )
 }
-
 export default App
