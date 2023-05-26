5e628c2df196706f3fb0a4421774794a56cd847c
John <sleepydude1993@gmail.com>
2023-05-26 16:46:45 -0500

    1.d funciton returions function

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -112,24 +112,55 @@
 //     </div>
 //   )
 // }
-import { useState  } from "react"
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
 
-  const handleLeftClick = () => 
-      setClicks({ ...clicks, left: clicks.left + 1})
- 
-  const handleRightCLick = () => 
-    setClicks({...clicks, right: clicks.right + 1 })
-  
+import { useState  } from "react"
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return(
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    setTotal(updateLeft + right)
+  } 
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  } 
+
     return(
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightCLick}>right</button>
-      {clicks.right}
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
