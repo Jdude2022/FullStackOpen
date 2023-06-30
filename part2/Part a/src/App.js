905531424ae3203c7574e307888731e1830743be
John <sleepydude1993@gmail.com>
2023-06-30 10:33:10 -0500

    start part 2

---
+++ part2/Part a/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const App = (props) => {
+  const {notes} = props
+  const result = notes.map(note => note.content)
+console.log(result)
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+           <li key={note.id}>
+            {note.content}
+           </li>
+        )}
+      </ul>
+     </div>
+  )
+}
+export default App;
+
