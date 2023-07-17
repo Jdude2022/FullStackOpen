7bd7f4dea0554c692724625c417941fa6aa83381
John <sleepydude1993@gmail.com>
2023-07-17 16:10:08 -0500

    2.2 COmplete

--- part2/course_information/src/App.js
+++ part2/course_information/src/App.js
@@ -1,4 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
+const Header = ({ course }) => <h1>{course.name}</h1> 
 
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
@@ -7,43 +7,58 @@ const Part = ({ part }) =>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Content = ({ parts }) => {
+  const result = parts.map(part => part.id)
+  console.log(result)
+  let total_ex = parts.map(part => part.exercises)
+  console.log(total_ex)
+  total_ex = total_ex.reduce(
+    (i, currentVal) => i + currentVal, 0
+  )
+  console.log(total_ex)
+  return(
+<>
+    {parts.map(part =>
+      <Part key={part.id} part={part} />)}
+    <Total sum={total_ex} />
+</>)
+}
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-  return (
+const Course = ({course}) => {
+  console.log(course.name)
+  return(
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Content parts={course.parts} />
     </div>
   )
+
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
